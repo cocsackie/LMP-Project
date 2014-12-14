1135d2a7dabc21b376e75b9324a0be64ab2d69f9
Maciej Trynkowski <trynkowm@len.iem.pw.edu.pl>
2014-12-14 03:46:20 +0100

    Poprawienie czytelności kodu aproksymatora legendre'a oraz poprawienie współczynników i usunięcie zbędnego kodu.

--- aproksymator_legendre.c
+++ aproksymator_legendre.c
@@ -17,9 +17,16 @@ double fi(double a, double b, int n, int i, double x)
 {	
 	long double sX = x;
 	long double sX2 = sX*sX;
-	long double sX4 = sX2*sX2;
-	long double sX8 = sX4*sX4;
-	
+	long double sX3 = sX2*sX;
+	long double sX4 = sX3*sX;
+	long double sX5 = sX4*sX;
+	long double sX6 = sX5*sX;
+	long double sX7 = sX6*sX;
+	long double sX8 = sX7*sX;
+	long double sX9 = sX8*sX;
+	long double sX10 = sX9*sX;
+	long double sX11 = sX10*sX;
+
 	switch( i )
 	{
 		case 0:
@@ -29,23 +36,23 @@ double fi(double a, double b, int n, int i, double x)
 		case 2:
 			return (3*sX2-1)/2;
 		case 3:
-			return (5*sX2*sX-3*sX)/2;
+			return (5*sX3-3*sX)/2;
 		case 4:
 			return (35*sX4-30*sX2+3)/8;
 		case 5:
-			return (63*sX4*sX-70*sX2*sX+15*sX)/8;
+			return (63*sX5-70*sX3+15*sX)/8;
 		case 6:
-			return (231*sX4*sX2-315*sX4+105*sX2-5)/16;
+			return (231*sX6-315*sX4+105*sX2-5)/16;
 		case 7:
-			return (429*sX4*sX2*sX-693*sX4*sX+315*sX2*sX-35*sX)/16;
+			return (429*sX7-693*sX5+315*sX3-35*sX)/16;
 		case 8:
-			return (6435*sX8-12012*sX4*sX2+6930*sX4-1260*sX2+35)/128;
+			return (6435*sX8-12012*sX6+6930*sX4-1260*sX2+35)/128;
 		case 9:
-			return (12155*sX8*sX-25740*sX4*sX2*sX+18018*sX4*sX-4620*sX2*sX+315*sX)/128;
+			return (12155*sX9-25740*sX7+18018*sX5-4620*sX3+315*sX)/128;
 		case 10:
-			return (46189*sX8*sX2-109395*sX8+90090*sX4*sX2-30030*sX4+3465*sX2-63)/256;
+			return (46189*sX10-109395*sX8+90090*sX6-30030*sX4+3465*sX2-63)/256;
 		case 11:
-			return (88179*sX8*sX2*sX-230945*sX8*sX+218790*sX4*sX2*sX-90090*sX4*sX+15015*sX2*sX-693*sX)/256;
+			return (88179*sX11-230945*sX9+218790*sX7-90090*sX5+15015*sX3-693*sX)/256;
 	}
 
 
@@ -53,13 +60,19 @@ double fi(double a, double b, int n, int i, double x)
 }
 
 /* Pierwsza pochodna fi */
-double
-dfi(double a, double b, int n, int i, double x)
+double dfi(double a, double b, int n, int i, double x)
 {
 	long double sX = x;
 	long double sX2 = sX*sX;
-	long double sX4 = sX2*sX2;
-	long double sX8 = sX4*sX4;
+	long double sX3 = sX2*sX;
+	long double sX4 = sX3*sX;
+	long double sX5 = sX4*sX;
+	long double sX6 = sX5*sX;
+	long double sX7 = sX6*sX;
+	long double sX8 = sX7*sX;
+	long double sX9 = sX8*sX;
+	long double sX10 = sX9*sX;
+	long double sX11 = sX10*sX;
 
 	switch( i )
 	{
@@ -72,34 +85,40 @@ dfi(double a, double b, int n, int i, double x)
 		case 3:
 			return (15*sX2-3)/2;
 		case 4:
-			return (140*sX2*sX-60*sX)/8;
+			return (140*sX3-60*sX)/8;
 		case 5:
 			return (315*sX4-210*sX2+15)/8;
 		case 6:
-			return (1386*sX4*sX-1260*sX2*sX+210*sX)/16;
+			return (1386*sX5-1260*sX3+210*sX)/16;
 		case 7:
-			return (3003*sX4*sX2-3465*sX4+945*sX2-35)/16;
+			return (3003*sX6-3465*sX4+945*sX2-35)/16;
 		case 8: 
-			return (51480*sX4*sX2*sX-72072*sX4*sX+27720*sX2*sX-2520)/128;
+			return (51480*sX7-72072*sX5+27720*sX3-2520*sX)/128;
 		case 9:
-			return (109395*sX8-180180*sX4*sX2+90090*sX4-13860*sX2+315)/128;
+			return (109395*sX8-180180*sX6+90090*sX4-13860*sX2+315)/128;
 		case 10:
-			return (461890*sX8*sX-875160*sX4*sX2*sX+540540*sX4*sX-120120*sX2*sX+6930*sX)/256;
+			return (461890*sX9-875160*sX7+540540*sX5-120120*sX3+6930*sX)/256;
 		case 11:
-			return (969969*sX8*sX2-2078505*sX8+901530*sX4*sX2-450450*sX4+45045*sX2-693)/256;
+			return (969969*sX10-2078505*sX8+1531530*sX6-450450*sX4+45045*sX2-693)/256;
 	}
 
 	return 0;
 }
 
 /* Druga pochodna fi */
-double
-d2fi(double a, double b, int n, int i, double x)
+double d2fi(double a, double b, int n, int i, double x)
 {
 	long double sX = x;
 	long double sX2 = sX*sX;
-	long double sX4 = sX2*sX2;
-	long double sX8 = sX4*sX4;
+	long double sX3 = sX2*sX;
+	long double sX4 = sX3*sX;
+	long double sX5 = sX4*sX;
+	long double sX6 = sX5*sX;
+	long double sX7 = sX6*sX;
+	long double sX8 = sX7*sX;
+	long double sX9 = sX8*sX;
+	long double sX10 = sX9*sX;
+	long double sX11 = sX10*sX;
 
 	switch( i )
 	{
@@ -114,32 +133,38 @@ d2fi(double a, double b, int n, int i, double x)
 		case 4:
 			return (420*sX2-60)/8;
 		case 5:
-			return (1260*sX2*sX-410*sX)/8;
+			return (1260*sX3-410*sX)/8;
 		case 6:
 			return (6930*sX4-3780*sX2+210)/16;
 		case 7:
-			return (18018*sX4*sX-13860*sX2*sX+1890*sX)/16;
+			return (18018*sX5-13860*sX3+1890*sX)/16;
 		case 8:
-			return (360360*sX4*sX2-360360*sX4+83160*sX2)/128;
+			return (360360*sX6-360360*sX4+83160*sX2-2520)/128;
 		case 9:
-			return (875160*sX4*sX2*sX-1081080*sX4*sX+360360*sX2*sX-27720)/128;
+			return (875160*sX7-1081080*sX5+360360*sX3-27720*sX)/128;
 		case 10:
-			return (4157010*sX8-6126120*sX4*sX2+2702700*sX4-360360*sX2+6930)/256;
+			return (4157010*sX8-6126120*sX6+2702700*sX4-360360*sX2+6930)/256;
 		case 11:
-			return (9699690*sX8*sX-16628040*sX4*sX2*sX+5409180*sX4*sX-1801800*sX2*sX+90090*sX)/256;
+			return (9699690*sX9-16628040*sX7+9189180*sX5-1801800*sX3+90090*sX)/256;
 	}
 
 	return 0;
 }
 
 /* Trzecia pochodna fi */
-double
-d3fi(double a, double b, int n, int i, double x)
+double d3fi(double a, double b, int n, int i, double x)
 {
 	long double sX = x;
 	long double sX2 = sX*sX;
-	long double sX4 = sX2*sX2;
-	long double sX8 = sX4*sX4;
+	long double sX3 = sX2*sX;
+	long double sX4 = sX3*sX;
+	long double sX5 = sX4*sX;
+	long double sX6 = sX5*sX;
+	long double sX7 = sX6*sX;
+	long double sX8 = sX7*sX;
+	long double sX9 = sX8*sX;
+	long double sX10 = sX9*sX;
+	long double sX11 = sX10*sX;
 
 	switch( i )
 	{
@@ -156,45 +181,23 @@ d3fi(double a, double b, int n, int i, double x)
 		case 5:
 			return (3780*sX2-410)/8;
 		case 6:
-			return (27720*sX2*sX-7560)/16;
+			return (27720*sX3-7560*sX)/16;
 		case 7:
 			return (90090*sX4-41580*sX2+1890)/16;
 		case 8:
-			return (2162160*sX4*sX-1441440*sX2*sX+166320*sX)/128;
+			return (2162160*sX5-1441440*sX3+166320*sX)/128;
 		case 9:
-			return (6126120*sX4*sX2-5405400*sX4+1081080*sX2*sX-720720*sX)/128;
+			return (6126120*sX6-5405400*sX4+1081080*sX2-27720)/128;
 		case 10:
-			return (33256080*sX4*sX2*sX-36756720*sX4*sX+1081080*sX2*sX-720720*sX)/256;
+			return (33256080*sX7-36756720*sX5+1081080*sX3-720720*sX)/256;
 		case 11:
-			return (87297210*sX8-116396280*sX4*sX2+27045900*sX4-5405400*sX2+90090)/256;
+			return (87297210*sX8-116396280*sX6+45945900*sX4-5405400*sX2+90090)/256;
 	}
 
 	return 0;
 }
-/* Pomocnicza f. do rysowania bazy */
-double
-xfi(double a, double b, int n, int i, FILE *out)
-{
-	double		h = (b - a) / (n - 1);
-	double		h3 = h * h * h;
-	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
-	double		hx      [5];
-	int		j;
 
-	for (j = 0; j < 5; j++)
-		hx[j] = a + h * hi[j];
-
-	fprintf( out, "# nb=%d, i=%d: hi=[", n, i );
-	for( j= 0; j < 5; j++ )
-		fprintf( out, " %d", hi[j] );
-	fprintf( out, "] hx=[" );
-	for( j= 0; j < 5; j++ )
-		fprintf( out, " %g", hx[j] );
-	fprintf( out, "]\n" );
-}
-
-void
-make_spl(points_t * pts, spline_t * spl)
+void make_spl(points_t * pts, spline_t * spl)
 {
 
 	matrix_t       *eqs= NULL;
@@ -211,27 +214,6 @@ make_spl(points_t * pts, spline_t * spl)
 
 	eqs = make_matrix(nb, nb + 1);
 
-#ifdef DEBUG
-#define TESTBASE 500
-	{
-		FILE           *tst = fopen("debug_base_plot.txt", "w");
-		double		dx = (b - a) / (TESTBASE - 1);
-		for( j= 0; j < nb; j++ )
-			xfi( a, b, nb, j, tst );
-		for (i = 0; i < TESTBASE; i++) {
-			fprintf(tst, "%g", a + i * dx);
-			for (j = 0; j < nb; j++) {
-				fprintf(tst, " %g", fi  (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", dfi (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d2fi(a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d3fi(a, b, nb, j, a + i * dx));
-			}
-			fprintf(tst, "\n");
-		}
-		fclose(tst);
-	}
-#endif
-
 	for (j = 0; j < nb; j++) {
 		for (i = 0; i < nb; i++)
 			for (k = 0; k < pts->n; k++)
@@ -241,17 +223,11 @@ make_spl(points_t * pts, spline_t * spl)
 			add_to_entry_matrix(eqs, j, nb, y[k] * fi(a, b, nb, j, x[k]));
 	}
 
-#ifdef DEBUG
-	write_matrix(eqs, stdout);
-#endif
 
 	if (piv_ge_solver(eqs)) {
 		spl->n = 0;
 		return;
 	}
-#ifdef DEBUG
-	write_matrix(eqs, stdout);
-#endif
 
 	if (alloc_spl(spl, pts->n) == 0) {
 		for (i = 0; i < spl->n; i++) {
@@ -270,27 +246,4 @@ make_spl(points_t * pts, spline_t * spl)
 			}
 		}
 	}
-
-#ifdef DEBUG
-	{
-		FILE           *tst = fopen("debug_spline_plot.txt", "w");
-		double		dx = (b - a) / (TESTBASE - 1);
-		for (i = 0; i < TESTBASE; i++) {
-			double yi= 0;
-			double dyi= 0;
-			double d2yi= 0;
-			double d3yi= 0;
-			double xi= a + i * dx;
-			for( k= 0; k < nb; k++ ) {
-							yi += get_entry_matrix(eqs, k, nb) * fi(a, b, nb, k, xi);
-							dyi += get_entry_matrix(eqs, k, nb) * dfi(a, b, nb, k, xi);
-							d2yi += get_entry_matrix(eqs, k, nb) * d2fi(a, b, nb, k, xi);
-							d3yi += get_entry_matrix(eqs, k, nb) * d3fi(a, b, nb, k, xi);
-			}
-			fprintf(tst, "%g %g %g %g %g\n", xi, yi, dyi, d2yi, d3yi );
-		}
-		fclose(tst);
-	}
-#endif
-
 }
